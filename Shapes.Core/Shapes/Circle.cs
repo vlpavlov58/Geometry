1415714a3ead688dbe4b0d90657f18d092710ee9
Pavlov Vladyslav <vlpavlov58@gmail.com>
2018-10-26 14:53:40 +0300

    Circle Class added

---
+++ Shapes.Core/Shapes/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Core.Shapes
+{
+    public class Circle : ShapeBase
+    {
+        public double Radius { get; set; }
+
+        public override double GetPerimetr()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override double GetSquare()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }    
+    }
+}
