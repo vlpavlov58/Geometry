f74dbe1dc052546c16247bb7218d4d9eb97ac109
Pavlov Vladyslav <vlpavlov58@gmail.com>
2018-10-26 15:17:23 +0300

    Class Ellipse added

---
+++ Shapes.Core/Shapes/Ellipse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Core.Shapes
+{
+    public class Ellipse : ShapeBase
+    {
+        public double Semiaxis1 { get; set; }
+        public double Semiaxis2 { get; set; }
+
+        public Ellipse(double semiaxis1, double semiaxis2)
+        {
+            this.Semiaxis1 = semiaxis1;
+            this.Semiaxis2 = semiaxis2;
+        }
+
+        public override double GetPerimetr()
+        {
+            return Math.PI * (this.Semiaxis1 + this.Semiaxis2);
+        }
+
+        public override double GetSquare()
+        {
+            return Math.PI * Semiaxis1 * Semiaxis2;
+        }
+    }
+}
